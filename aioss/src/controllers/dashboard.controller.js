6faa9183edb0868a11513890c14e2732fe928b74
TusharNugen <tusharthakur@nugeninfo.com>
2025-06-05 17:23:05 +0530

    updated org.josn

--- aioss/src/controllers/dashboard.controller.js
+++ aioss/src/controllers/dashboard.controller.js
@@ -1,15 +1,15 @@
-import log from '../config/logger.js';
-import orgJson from '../constants/organizationCreation.json' assert { type: 'json' };
-import layout from '../constants/layout.json' assert { type: 'json' };
-import dashboard from '../constants/dashboard.json' assert { type: 'json' };
-import sendResponse from '../../../organization/src/utils/response.handler.js';
+import log from "../config/logger.js";
+import orgJson from "../constants/organizationCreation.json" assert { type: "json" };
+import layout from "../constants/layout.json" assert { type: "json" };
+import dashboard from "../constants/dashboard.json" assert { type: "json" };
+import sendResponse from "../../../organization/src/utils/response.handler.js";
 
 export const getCreateOrganizationJson = async (req, res) => {
   try {
     const json = orgJson;
     return sendResponse(res, {
-      data: json.orgJson,
-      message: 'Create organization JSON send successfully',
+      data: json,
+      message: "Create organization JSON send successfully",
     });
   } catch (err) {
     log.error(`Get create organization JSON error: ${err.message}`);
@@ -22,7 +22,7 @@ export const getDashboardData = async (req, res) => {
     const json = dashboard;
     return sendResponse(res, {
       data: json,
-      message: 'Create organization JSON send successfully',
+      message: "Create organization JSON send successfully",
     });
   } catch (err) {
     log.error(`Get create organization JSON error: ${err.message}`);
@@ -35,7 +35,7 @@ export const getlayoutData = async (req, res) => {
     const json = layout;
     return sendResponse(res, {
       data: json,
-      message: 'Create organization JSON send successfully',
+      message: "Create organization JSON send successfully",
     });
   } catch (err) {
     log.error(`Get create organization JSON error: ${err.message}`);

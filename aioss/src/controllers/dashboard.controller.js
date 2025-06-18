e2ccc8de62b7c2924ec57c0c2d6d5f4e9b859137
TusharNugen <tusharthakur@nugeninfo.com>
2025-06-18 12:50:06 +0530

    code-update

--- aioss/src/controllers/dashboard.controller.js
+++ aioss/src/controllers/dashboard.controller.js
@@ -1,44 +1,41 @@
 import log from "../config/logger.js";
-import orgJson from "../constants/organizationCreation.json" assert { type: "json" };
-import layout from "../constants/layout.json" assert { type: "json" };
-import dashboard from "../constants/dashboard.json" assert { type: "json" };
-import sendResponse from "../../../organization/src/utils/response.handler.js";
+import { DashboardService } from "../service/dashboard.service.js";
 
 export const getCreateOrganizationJson = async (req, res) => {
   try {
-    const json = orgJson;
-    return sendResponse(res, {
+    const json = DashboardService.getCreateOrganizatoinJson();
+    return res.status(200).json({
       data: json,
       message: "Create organization JSON send successfully",
     });
   } catch (err) {
     log.error(`Get create organization JSON error: ${err.message}`);
-    return sendResponse(res, { error: err });
+    return res.status(500).json({ error: err.message });
   }
 };
 
 export const getDashboardData = async (req, res) => {
   try {
-    const json = dashboard;
-    return sendResponse(res, {
-      data: json,
-      message: "Create organization JSON send successfully",
+    const json = DashboardService.getDashboardJson();
+    return res.status(200).json({
+      data: json.dashboard,
+      message: "Organization JSON send successfully",
     });
   } catch (err) {
     log.error(`Get create organization JSON error: ${err.message}`);
-    return sendResponse(res, { error: err });
+    return res.status(500).json({ error: err.message });
   }
 };
 
 export const getlayoutData = async (req, res) => {
   try {
-    const json = layout;
-    return sendResponse(res, {
+    const json = DashboardService.getLayoutJson();
+    return res.status(200).json({
       data: json,
       message: "Create organization JSON send successfully",
     });
   } catch (err) {
     log.error(`Get create organization JSON error: ${err.message}`);
-    return sendResponse(res, { error: err });
+    return res.status(500).json({ error: err.message });
   }
 };
--- api-gateway/app.js
+++ api-gateway/app.js
@@ -1,20 +1,21 @@
-import express from "express";
-import morgan from "morgan";
-import { createProxyMiddleware } from "http-proxy-middleware";
+import express from 'express';
+import morgan from 'morgan';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const app = express();
 
 const routes = {
-  "/auth": "http://localhost:2001",
-  "/org": "http://localhost:2002",
-  "/core": "http://localhost:2003",
+  '/auth': 'http://localhost:2001',
+  '/org': 'http://localhost:2002',
+  '/core': 'http://localhost:2003',
+  '/chat': 'http://localhost:2004',
 };
 
 for (const route in routes) {
   const target = routes[route];
   app.use(route, createProxyMiddleware({ target }));
-  app.use(morgan("dev"));
-  app.get("/health-check", (req, res) => {
+  app.use(morgan('dev'));
+  app.get('/health-check', (req, res) => {
     res.status(200).json({
       message: `api gateway service is running`,
       success: true,
@@ -22,4 +23,4 @@ for (const route in routes) {
   });
 }
 
-app.listen(9009, () => console.log("API Gateway Started on port 9009"));
+app.listen(9009, () => console.log('API Gateway Started on port 9009'));
